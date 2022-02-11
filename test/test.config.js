91e141123d6115b52f9d8df5b8279951eda1de30
Dave Longley <dlongley@digitalbazaar.com>
2022-02-11 16:09:03 -0500

    Add database deps and KMS test deps.

--- test/test.config.js
+++ test/test.config.js
@@ -5,6 +5,16 @@
 
 const {config} = require('bedrock');
 const path = require('path');
+require('bedrock-service-agent');
+
+// MongoDB
+config.mongodb.name = 'bedrock_service_agent_test';
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
+// drop all collections on initialization
+config.mongodb.dropCollections = {};
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
 
 config.mocha.tests.push(path.join(__dirname, 'mocha'));
 
