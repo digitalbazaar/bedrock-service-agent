42d89fd4dddd50a241264087c4a42e2b7f11522f
Dave Longley <dlongley@digitalbazaar.com>
2022-02-11 15:52:26 -0500

    Add initial files.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# bedrock-module-template-http ChangeLog
+# bedrock-service-agent ChangeLog
 
 ## 1.0.0 - TBD
 
--- test/test.config.js
+++ test/test.config.js
@@ -1,15 +1,10 @@
-/*
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
 const {config} = require('bedrock');
 const path = require('path');
-
-// MongoDB
-config.mongodb.name = 'bedrock_module_template_http_test';
-config.mongodb.dropCollections.onInit = true;
-config.mongodb.dropCollections.collections = [];
 
 config.mocha.tests.push(path.join(__dirname, 'mocha'));
 
