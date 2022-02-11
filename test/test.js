5724fd0f84904e4084923f209046a6e15082c146
Dave Longley <dlongley@digitalbazaar.com>
2022-02-11 15:52:26 -0500

    Add initial files.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,37 @@
-/*
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
 const bedrock = require('bedrock');
 require('bedrock-https-agent');
-require('bedrock-mongodb');
-require('bedrock-module-template-http');
-
 require('bedrock-test');
+require('bedrock-service-agent');
+
+const mockData = require('./mocha/mock.data');
+
+bedrock.events.on('bedrock.init', async () => {
+  /* Handlers need to be added before `bedrock.start` is called. These are
+  no-op handlers to enable meter usage without restriction */
+  handlers.setCreateHandler({
+    handler({meter} = {}) {
+      // use configured meter usage reporter as service ID for tests
+      const clientName = mockData.productIdMap.get(meter.product.id);
+      const serviceIdentites = getServiceIdentities();
+      const serviceIdentity = serviceIdentites.get(clientName);
+      if(!serviceIdentity) {
+        throw new Error(`Could not find identity "${clientName}".`);
+      }
+      meter.serviceId = serviceIdentity.id;
+      return {meter};
+    }
+  });
+  handlers.setUpdateHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setRemoveHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setUseHandler({handler: ({meter} = {}) => ({meter})});
+
+  // FIXME: call `createService`
+  //await createService({});
+});
+
 bedrock.start();
