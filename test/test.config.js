c0aad915c1c2bb8d9b898ec2cec08ce763151c2b
Dave Longley <dlongley@digitalbazaar.com>
2022-02-15 16:43:07 -0500

    Start adding http tests.

--- test/test.config.js
+++ test/test.config.js
@@ -20,3 +20,15 @@ config.mocha.tests.push(path.join(__dirname, 'mocha'));
 
 // allow self-signed certs in test framework
 config['https-agent'].rejectUnauthorized = false;
+
+// create test application identity
+// ...and `ensureConfigOverride` has already been set via
+// `bedrock-app-identity` so it doesn't have to be set here
+config['app-identity'].seeds.services.example = {
+  id: 'did:key:z6MkrH839XwPCUQ2TkA6ifehciWnEvzuQ2njc6J19fpuP5oN',
+  seedMultibase: 'z1AgvAGfbairK3AV6GqbeF8gSpYZXftQsGb5DTjptgawNyn',
+  serviceType: 'example'
+};
+
+// use local KMS for testing
+config['service-agent'].kms.baseUrl = 'https://localhost:18443/kms';
--- test/test.js
+++ test/test.js
@@ -5,18 +5,18 @@
 
 const bedrock = require('bedrock');
 const {getServiceIdentities} = require('bedrock-app-identity');
+require('bedrock-edv-storage');
 require('bedrock-https-agent');
 require('bedrock-kms');
 require('bedrock-kms-http');
 require('bedrock-meter');
 require('bedrock-meter-usage-reporter');
 const {handlers} = require('bedrock-meter-http');
-require('bedrock-mongodb');
 require('bedrock-server');
-require('bedrock-service-agent');
-// this is responsible for providing the `ssm-v1` key store
+const {initializeServiceAgent} = require('bedrock-service-agent');
 require('bedrock-ssm-mongodb');
 require('bedrock-test');
+const {createService} = require('bedrock-service-core');
 
 const mockData = require('./mocha/mock.data');
 
@@ -39,6 +39,26 @@ bedrock.events.on('bedrock.init', async () => {
   handlers.setUpdateHandler({handler: ({meter} = {}) => ({meter})});
   handlers.setRemoveHandler({handler: ({meter} = {}) => ({meter})});
   handlers.setUseHandler({handler: ({meter} = {}) => ({meter})});
+
+  // create `example` service
+  /*const service =*/await createService({
+    serviceType: 'example',
+    routePrefix: '/examples',
+    storageCost: {
+      config: 1,
+      revocation: 1
+    },
+    // require these zcaps (by reference ID)
+    zcapReferenceIds: ['edv', 'hmac', 'keyAgreementKey']
+  });
+});
+
+// normally a service agent should be created on `bedrock-mongodb.ready`,
+// however, since the KMS system used is local, we have to wait for it to
+// be ready; so only do this on `bedrock.ready`
+bedrock.events.on('bedrock.ready', async () => {
+  // initialize service agent
+  await initializeServiceAgent({serviceType: 'example'});
 });
 
 bedrock.start();
